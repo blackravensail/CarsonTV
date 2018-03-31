8d5a75fbe5fedf0a41c8f1744aad51ffdfccc4bc
unknown <blakewintermute18>
2018-03-31 14:31:40 -0400

    Adding SKRN based first version

---
+++ js/customScripts/temp.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+
+
+"<li class='progression_studios_animate_left'><div class='progression-studios-slider-dashboard-image-background' style='background-image:url("+wideImg+");'><div class='progression-studios-slider-display-table'><div class='progression-studios-slider-vertical-align'><div class='container'><a class='progression-studios-slider-play-btn afterglow' href='#VideoLightbox-1'><i class='fas fa-play'></i></a><video id='VideoLightbox-1' width='960' height='540'><source src='"trailer"' type='video/mp4'></video><div class='circle-rating-pro' data-value='"+rating/10+"' data-animation-start-value='"+rating/10+"' data-size='70' data-thickness='6' data-fill='{&quot;color&quot;: &quot;"+ratingColor+"&quot;}' data-empty-fill='#def6de' data-reverse='true'><span style='color:#42b740;'>"+rating+"</span></div><div class='progression-studios-slider-dashboard-caption-width'><div class='progression-studios-slider-caption-align'><h6>"+type+"</h6><ul class='progression-studios-slider-rating'>"+tags+"</ul><h2><a href='dashboard-movie-profile.html?type="+type+"&id="+id+"'>"+name+"</a></h2><ul class='progression-studios-slider-meta'><li>"+releaseData+"</li><li>"+lenght+" min.</li><li>"+genres+"</li></ul><p class='progression-studios-slider-description'>"+description+"</p><a class='btn btn-green-pro btn-slider-pro btn-shadow-pro afterglow' href='#VideoLightbox-1'><i class='fas fa-play'></i> Watch Trailer</a><div class='clearfix'></div></div></div></div></div></div><div class='progression-studios-slider-mobile-background-cover'></div></div></li>"
