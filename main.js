4aa70c90c62c7f7256a87304298825f3819ecf77
unknown <blakewintermute18>
2018-04-07 23:24:04 -0400

    Fixed Banner not rendering, made random change every 100secs

--- main.js
+++ main.js
@@ -1,109 +1,117 @@
 header = "https://ipfs.io/ipfs/"
 //header = "http://127.0.0.1:8080/ipfs/"
-var data;
 var slider;
 var titleCont;
-$.getJSON("./DataGenerator/fileDump.json", function(json) {
-    data = json
-    var tl = data["series"]
-    tl = tl.concat(data["movies"])
-    var rand = Math.floor(Math.random() * tl.length)
-    slider = new Vue({
-        el: "#vue_slider",
-        data:{
-            title:tl[rand],
-        },
-        methods: {
-            getBackgroundStyle: function(str) {
-                return "background-image:url("+header + str+");"
-            },
-            getType: function(){
-                if (this.title.hasOwnProperty("main_id")) {
-                    return "Film"
-                }
-                else {
-                    return "TV Show"
-                }
-            },
-            getReticle: function(rating) {
-                if (rating > 6){
-                    ratingColor = "#42b740"
-                    ratingColorBack = "#def6de"
-                }
-                else{
-                    ratingColor = "#FF4343"
-                    ratingColorBack = "FFEBEB"
-                }
+td = data["series"]
+td = td.concat(data["movies"])
 
-                return "<div class='circle-rating-pro' data-value='"+rating/10+"' data-animation-start-value='"+rating/10+"' data-size='70' data-thickness='6' data-fill='{&quot;color&quot;: &quot;"+ratingColor+"&quot;}' data-empty-fill='"+ratingColorBack+"' data-reverse='true'><span style='color:"+ratingColor+";'>"+rating+"</span></div>"
-                //return "<div class='circle-rating-pro' data-value='0.82' data-animation-start-value='0.82' data-size="70" data-thickness="6" data-fill="{&quot;color&quot;: &quot;#42b740&quot;}" data-empty-fill="#203521" data-reverse="true"><span style="color:#42b740;">8.2</span></div>"
-            },
-            getColor: function(rating) {
-                if (rating > 7){return "green"}
-                else if (rating > 5){return "orange"}
-                else {return "red"}
-            },
-            getAddress: function() {
-                if(this.title.hasOwnProperty("id")){
-                    return "view.html?id="+this.title["id"]
-                }
-                else {
-                    return "view.html?id="+this.title["title"]
-                }
+
+var rand = Math.floor((new Date()).getTime() / 100000) % td.length
+var sliderTitle = td[rand]
+
+
+console.log(sliderTitle)
+slider = new Vue({
+    el: "#vue_slider",
+    data: {
+        title: sliderTitle,
+    },
+    methods: {
+        getBackgroundStyle: function(str) {
+            return "background-image:url(" + header + str + ");"
+        },
+        getType: function() {
+            if (this.title.hasOwnProperty("main_id")) {
+                return "Film"
+            } else {
+                return "TV Show"
             }
-        }
-    })
-
-    titleCont = new Vue({
-        el:"#pannelCont",
-        data:{
-            titles:data["series"],
-            header: "TV Shows"
         },
-        methods: {
-            addheader: function(str) {
-                return header + str
-            },
-            getColorHex: function(rating, bool) {
-                if (rating > 6){
-                    ratingColor = "#42b740"
-                    ratingColorBack = "#def6de"
-                }
-                else{
-                    ratingColor = "#FF4343"
-                    ratingColorBack = "FFEBEB"
-                }
+        getReticle: function(rating) {
+            if (rating > 6) {
+                ratingColor = "#42b740"
+                ratingColorBack = "#def6de"
+            } else {
+                ratingColor = "#FF4343"
+                ratingColorBack = "FFEBEB"
+            }
 
-                if(bool){return ratingColor}
-                else{return ratingColorBack}
-            },
-            getColor: function(rating) {
-                if (rating > 7){return "green"}
-                if (rating > 5){return "orange"}
+            return "<div class='circle-rating-pro' data-value='" + rating / 10 + "' data-animation-start-value='" + rating / 10 + "' data-size='70' data-thickness='6' data-fill='{&quot;color&quot;: &quot;" + ratingColor + "&quot;}' data-empty-fill='" + ratingColorBack + "' data-reverse='true'><span style='color:" + ratingColor + ";'>" + rating + "</span></div>"
+            //return "<div class='circle-rating-pro' data-value='0.82' data-animation-start-value='0.82' data-size="70" data-thickness="6" data-fill="{&quot;color&quot;: &quot;#42b740&quot;}" data-empty-fill="#203521" data-reverse="true"><span style="color:#42b740;">8.2</span></div>"
+        },
+        getColor: function(rating) {
+            if (rating > 7) {
+                return "green"
+            } else if (rating > 5) {
+                return "orange"
+            } else {
                 return "red"
-            },
-            getaddress: function(obj) {
-                if(obj.hasOwnProperty("id")){
-                    return "view.html?id="+obj["id"]
-                }
-                else {
-                    return "view.html?id="+obj["title"]
-                }
+            }
+        },
+        getAddress: function() {
+            if (this.title.hasOwnProperty("id")) {
+                return "view.html?id=" + this.title["id"]
+            } else {
+                return "view.html?id=" + this.title["title"]
             }
         }
-    })
-    //search("killer", true, true, "Drama", 4, 10)
-});
+    }
+})
+
+titleCont = new Vue({
+    el: "#pannelCont",
+    data: {
+        titles: data["series"],
+        header: "TV Shows"
+    },
+    methods: {
+        addheader: function(str) {
+            return header + str
+        },
+        getColorHex: function(rating, bool) {
+            if (rating > 6) {
+                ratingColor = "#42b740"
+                ratingColorBack = "#def6de"
+            } else {
+                ratingColor = "#FF4343"
+                ratingColorBack = "FFEBEB"
+            }
+
+            if (bool) {
+                return ratingColor
+            } else {
+                return ratingColorBack
+            }
+        },
+        getColor: function(rating) {
+            if (rating > 7) {
+                return "green"
+            }
+            if (rating > 5) {
+                return "orange"
+            }
+            return "red"
+        },
+        getaddress: function(obj) {
+            if (obj.hasOwnProperty("id")) {
+                return "view.html?id=" + obj["id"]
+            } else {
+                return "view.html?id=" + obj["title"]
+            }
+        }
+    }
+})
+
+//search("killer", true, true, "Drama", 4, 10)
 
 
 $(".men_item").on('click', function() {
     $(".men_item").removeClass("current-menu-item")
     $(this).addClass("current-menu-item")
-    if ($(this).find("a").text().replace(/\s/g,'') == "Movies") {
+    if ($(this).find("a").text().replace(/\s/g, '') == "Movies") {
         titleCont.titles = data["movies"]
         titleCont.header = "Movies"
-    }
-    else{
+    } else {
         titleCont.titles = data["series"]
         titleCont.header = "TV Shows"
     }
@@ -113,15 +121,15 @@ $(".men_item").on('click', function() {
 $(".genre_button").on("click", function() {
     $(".genre_button").removeClass("active")
     $(this).addClass("active")
-    var genre = $(this).find("h6").text().replace(/\s/g,'')
+    var genre = $(this).find("h6").text().replace(/\s/g, '')
     var list = []
-    for (var i = 0; i<data["movies"].length; i++) {
+    for (var i = 0; i < data["movies"].length; i++) {
         if (data["movies"][i]["genres"].indexOf(genre) >= 0) {
             list.push(data["movies"][i])
         }
     }
-    for (var i = 0; i<data["series"].length; i++) {
-        if (data["series"][i]["genres"].indexOf(genre) >= 0){
+    for (var i = 0; i < data["series"].length; i++) {
+        if (data["series"][i]["genres"].indexOf(genre) >= 0) {
             list.push(data["series"][i])
         }
     }
--- view.js
+++ view.js
@@ -10,62 +10,89 @@ var main_area
 var player
 
 $("#seriesCont").hide()
-
-$.getJSON("./DataGenerator/fileDump.json", function(json) {
-    data = json
-    var tl = data["series"]
-    tl = tl.concat(data["movies"])
-    for (var i = 0; i < tl.length; i++) {
-        if (tl[i]["title"] == id) {
-            main_obj = tl[i];
-            sidebar = new Vue({
-                el:"#content-sidebar-pro",
-                data:{
-                    title:main_obj,
-                    header:header
-                }
-            })
-            main_area = new Vue({
-                el:"#col-main-with-sidebar",
-                data:{
-                    title:main_obj,
-                    epMap:main_obj["ep_map"],
-                    currentSeasonName: "",
-                    currentSeason: [],
-                    header:header
-                },
-                methods : {
-                    getColor: function(rating) {
-                        if (rating > 7){return "green"}
-                        if (rating > 5){return "orange"}
-                        return "red"
-                    },
-                    changeVideo: function(id) {
-                        console.log("HIIII")
-                        console.log(id)
-                        player.source ={
-                            type:'video',
-                            title: "",
-                            sources: [{
-                                src: header + id,
-                                type: 'video/mp4'
-                            }]
-                        }
-                        player.ratio = "16:9"
+var tl = data["series"]
+tl = tl.concat(data["movies"])
+for (var i = 0; i < tl.length; i++) {
+    if (tl[i]["title"] == id) {
+        main_obj = tl[i];
+        sidebar = new Vue({
+            el: "#content-sidebar-pro",
+            data: {
+                title: main_obj,
+                header: header
+            }
+        })
+        main_area = new Vue({
+            el: "#col-main-with-sidebar",
+            data: {
+                title: main_obj,
+                epMap: main_obj["ep_map"],
+                currentSeasonName: "",
+                currentSeason: [],
+                header: header
+            },
+            methods: {
+                getColor: function(rating) {
+                    if (rating > 7) {
+                        return "green"
                     }
+                    if (rating > 5) {
+                        return "orange"
+                    }
+                    return "red"
+                },
+                changeVideo: function(id) {
+                    console.log("HIIII")
+                    console.log(id)
+                    player.source = {
+                        type: 'video',
+                        title: "",
+                        sources: [{
+                            src: header + id,
+                            type: 'video/mp4'
+                        }]
+                    }
+                    player.ratio = "16:9"
                 }
-            })
-        }
+            }
+        })
     }
-    player = new Plyr("#main_video")
+}
+player = new Plyr("#main_video")
+player.ratio = "16:9"
+window.rr = player
+
+
+if (main_obj.hasOwnProperty("ep_map")) {
+    renderSeasons(main_obj["ep_map"])
+    player.source = {
+        type: 'video',
+        title: main_obj["title"],
+        sources: [{
+            src: header + main_obj["trailer_id"],
+            type: 'video/mp4'
+        }]
+    }
     player.ratio = "16:9"
-    window.rr = player
+    $("#trailer_Button").hide()
+} else {
+    player.source = {
+        type: 'video',
+        title: main_obj["title"],
+        sources: [{
+            src: header + main_obj["main_id"],
+            type: 'video/mp4'
+        }]
+    }
+    player.ratio = "16:9"
+}
+player.poster = header + main_obj["wide"]
+player.ratio = "16:9"
 
-
-    if (main_obj.hasOwnProperty("ep_map")){
-        renderSeasons(main_obj["ep_map"])
-        player.source ={
-            type:'video',
+$("#trailer_Button").on("click", function() {
+    if (j % 2 == 0) {
+        player.source = {
+            type: 'video',
             title: main_obj["title"],
             sources: [{
                 src: header + main_obj["trailer_id"],
@@ -73,11 +100,10 @@ $.getJSON("./DataGenerator/fileDump.json", function(json) {
             }]
         }
         player.ratio = "16:9"
-        $("#trailer_Button").hide()
-    }
-    else {
-        player.source ={
-            type:'video',
+        $("#trailer_Button").html("<i class='fas fa-play'></i>Watch Film")
+    } else {
+        player.source = {
+            type: 'video',
             title: main_obj["title"],
             sources: [{
                 src: header + main_obj["main_id"],
@@ -85,38 +111,9 @@ $.getJSON("./DataGenerator/fileDump.json", function(json) {
             }]
         }
         player.ratio = "16:9"
+        $("#trailer_Button").html("<i class='fas fa-play'></i>Watch Trailer")
     }
-    player.poster = header + main_obj["wide"]
-    player.ratio = "16:9"
-
-    $("#trailer_Button").on("click", function() {
-        if( j%2 == 0){
-            player.source ={
-                type:'video',
-                title: main_obj["title"],
-                sources: [{
-                    src: header + main_obj["trailer_id"],
-                    type: 'video/mp4'
-                }]
-            }
-            player.ratio = "16:9"
-            $("#trailer_Button").html("<i class='fas fa-play'></i>Watch Film")
-        }
-        else{
-            player.source ={
-                type:'video',
-                title: main_obj["title"],
-                sources: [{
-                    src: header + main_obj["main_id"],
-                    type: 'video/mp4'
-                }]
-            }
-            player.ratio = "16:9"
-            $("#trailer_Button").html("<i class='fas fa-play'></i>Watch Trailer")
-        }
-        j++;
-    })
-
+    j++;
 })
 
 function renderSeasons(ep_map) {
@@ -132,17 +129,18 @@ function renderSeasons(ep_map) {
     })
     window.rr = $(".episode")
     $(".episode").on('click', function() {
-            console.log("HIIIIIIII")
-        })
+        console.log("HIIIIIIII")
+    })
 
     $("#seriesCont").show()
 
 }
+
 function changeVideo(id) {
     console.log("HIIII")
     console.log(id)
-    player.source ={
-        type:'video',
+    player.source = {
+        type: 'video',
         title: "",
         sources: [{
             src: header + id,
