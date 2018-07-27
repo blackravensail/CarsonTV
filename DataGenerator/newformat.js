7df44eb9a698527acfd8d658330a24e7451dfeab
Blake Wintermute <blakewintermute@gmail.com>
2018-07-27 18:33:46 +0100

    Added New Format file

---
+++ DataGenerator/newformat.js
@@ -0,0 +1,66 @@
+data = {
+    "tt6769208": {
+        "type":"tv_show",
+        "description": "David Attenborough returns to the world's oceans in this sequel to the acclaimed documentary filming rare and unusual creatures of the deep, as well as documenting the problems our oceans face.",
+        "progress": 0,
+        "genres": [
+            "Documentary"
+        ],
+        "ep_map": [
+            {
+                "episodes": [
+                    {
+                        "title": "One Ocean",
+                        "video": "season/episode/video.mp4",
+                        "progress": 0,
+                        "image": "season/episode/tall.jpg",
+                        "captions": {
+                            "English": "lksjflk",
+                            "Español": "lkdfj",
+                            "Français": "lsdfkl"
+                        }
+                    }
+                ],
+                "title": "Season 1"
+            }
+        ],
+        "tall": "zb2rhXXdawTPgvKXoUwC97HtzGpg8eakDpvnVeaLpbFZ6jK2p",
+        "location": {
+            "ipfs": "lksdjfklskdjfk",
+            "html": [
+                "192.168.1.1:6000/CarsonMedia/Blue Planet II"
+            ]
+        },
+        "title": "Blue Planet II",
+        "release_date": "October 21, 2017",
+        "rating": 9.4,
+        "cE": 0,
+        "wide": "zb2rhdmZq1uwQi449HrTebBW8CGku2usejQN5C7peuVpNz3yP",
+        "cS": 0,
+        "duration": 60
+    },
+    "tt0327137": {
+        "type":"film",
+        "description": "A coming-of-age story about a shy, young boy sent by his irresponsible mother to spend the summer with his wealthy, eccentric uncles in Texas.",
+        "progress": 0,
+        "genres": [
+            "Comedy",
+            "Drama",
+            "Family"
+        ],
+        "wide": "wide.jpg",
+        "trailer": "video.trailer",
+        "title": "Secondhand Lions",
+        "duration": 111,
+        "rating": 7.6,
+        "feature": "mail.mp4",
+        "tall": "tall.jpg",
+        "release_date": "October 21, 2003",
+        "location": {
+            "ipfs": "lksdjfklskdjfk",
+            "html": [
+                "192.168.1.1:6000/CarsonMedia/Secondhand Lions"
+            ]
+        }
+    }
+}
